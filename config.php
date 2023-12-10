4b89c9c6a2bda21b097a9705b9e5219e3b1ed2d6
Adhitama Wichaksono <122053886+adhitamaw@users.noreply.github.com>
2023-12-11 03:55:21 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$database = "reservasilapangan"; #isi sesuai yang ada di mysql database yang dibuat
+$username = "root";
+$password = "";
+$host = "localhost:3307"; #sesuaikan dengan port xammp anda
+$conn = mysqli_connect($host, $username, $password, $database);
+
